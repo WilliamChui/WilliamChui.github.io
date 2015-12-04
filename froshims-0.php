d843fc5cbcb718b33b194bedb844a2c6228baae5
WilliamChui <peterread@126.com>
2015-12-04 17:01:27 +0800

    First try on php

---
+++ froshims-0.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Frosh IMs</title>
+	</head>
+	<body style="text-align: center;">
+		<h1>Register for Frosh IMs</h1>
+		<form action="register-0.html", method="post">
+			Name: <input name="name" type="text"/>
+			<br/>
+			<input name="captain" type="checkbox"/> Captain?
+			<br/>
+			<input name="gender" type="radio" value="F"/> Female
+			<input name="gender" type="radio" value="M"/> Male
+			<br/>
+			Dorm:
+			<select name="dorm">
+				<option value=""></option>
+				<option value="Apley Court">Apley Court</option>
+				<option value="Canaday">Canaday</option>
+				<option value="Lionel">Lionel</option>
+			</select>
+			<br/>
+			<input type="submit" value="Register"/>
+		</form>
+	</body>
+</html>
